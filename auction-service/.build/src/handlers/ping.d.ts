a6fd7775bbcb769540acf471d48e919b448b93ee
Ajitesh Deb <ajitesh.deb@gmail.com>
2022-01-30 23:32:03 +0530

    serverless deploy done

--- auction-service/.build/src/handlers/ping.d.ts
+++
@@ -1,3 +0,0 @@
-import { APIGatewayProxyEvent, Callback, Context } from 'aws-lambda';
-import { LambdaResponse } from '../types/lambdaResponse.type';
-export declare const ping: (event: APIGatewayProxyEvent, context: Context, callback: Callback) => Promise<LambdaResponse>;
--- auction-service/.build/src/types/lambdaResponse.type.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type LambdaResponse = {
-    headers: HeaderOption;
-    statusCode: number;
-    body: string;
-};
-interface HeaderOption {
-    'Access-Control-Allow-Origin': string;
-}
-export {};
